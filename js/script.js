b9b304f9782c22445f5dafb77c66e83de3883fc8
Karen Araujo <barbiekarenalves@outlook.com>
2026-08-21 20:48:20 -0300

    implementing the API in the project

--- js/script.js
+++ js/script.js
@@ -16,7 +16,29 @@ async function buscarFilme(event) { // Função para buscar filmes
 
     const query = encodeURIComponent(input.value);  // Codifica a pesquisa para ser usada na URL
     const url = `https://api.themoviedb.org/3/search/movie?query=${query}`; // URL da API para buscar filmes
-
+    try{
+        const options = { 
+            method: 'GET', 
+            headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer .....'
+            }
+        };
+        const response = await fetch(url, options); // Faz a requisição para a API
+        const data = await response.json(); // Converte a resposta em JSON
+        resultsContainer.innerHTML = ""; // Limpa os resultados anteriores
+        data.results.forEach((filme) => {
+            const movieCard = document.createElement("div");
+            movieCard.classList.add("movie-card");
+            movieCard.innerHTML = `
+                <h3>${filme.title}</h3>
+                <p>${filme.overview}</p>
+            `;
+            resultsContainer.appendChild(movieCard);
+        });
+    } catch (erro) {// Captura qualquer erro que ocorra durante a requisição ou processamento dos dados
+        console.error('Erro ao buscar o filme:', erro);// Exibe mensagem de erro no console caso ocorra algum problema na requisição
+        resultsContainer.innerHTML = `<p>Erro ao conectar com a API. Tente novamente mais tarde.</p>`;
+    }
 }
-
 form.addEventListener('submit', buscarFilme);
