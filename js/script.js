3adae3e21e5384c67dd37373d34285a8fd6a95e9
Karen Araujo <barbiekarenalves@outlook.com>
2026-08-20 22:09:24 -0300

    adding form validation and the TMDB API URL to the project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 const form = document.querySelector('#search-form');
 const input = document.querySelector('#movie-search');
+const resultsContainer = document.querySelector('#results');
 
-async function buscarFilme(event) {
-    event.preventDefault();
-    console.log("O formulário foi enviado!");
+async function buscarFilme(event) { // Função para buscar filmes
+    event.preventDefault(); // Impede o envio do formulário
+
+    input.value = input.value.trim(); // Remove espaços em branco no início e no final da pesquisa
+
+    if (!input.value) { // Verifica se o campo de pesquisa está vazio
+        resultsContainer.innerHTML = `
+            <p>Por favor, digite o nome de um filme.</p>
+        `;
+        return;
+    }
+
+    const query = encodeURIComponent(input.value);  // Codifica a pesquisa para ser usada na URL
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}`; // URL da API para buscar filmes
+
 }
 
 form.addEventListener('submit', buscarFilme);
