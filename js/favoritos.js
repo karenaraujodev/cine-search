26697e3bf998630f83eb5d429c333e683562ccad
Karen Araujo <barbiekarenalves@outlook.com>
2026-09-18 20:39:44 -0300

    feat: adiciona pôsteres aos filmes favoritos

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,16 +1,37 @@
 // Seleciona o elemento HTML onde os favoritos serão exibidos
 const favoritesContainer = document.querySelector("#favorites-container");
 
-console.log(favoritesContainer);
-
-
-// Pega os favoritos salvos no navegador
+// Pega os favoritos salvos
 const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
-
-console.log(favoritos);
-
-
-// Percorre cada filme salvo nos favoritos
+const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+// Percorre cada filme
 favoritos.forEach((filme) => {
+
     console.log(filme);
+
+    const movieCard = document.createElement("div");
+
+    movieCard.classList.add("movie-card");
+
+    const poster = filme.poster_path
+        ? `${imageBaseUrl}${filme.poster_path}`
+        : null;
+
+    const year = filme.release_date
+        ? filme.release_date.slice(0, 4)
+        : "Ano não informado";
+
+    const overview = filme.overview
+        ? filme.overview
+        : "Sinopse não disponível.";
+
+    movieCard.innerHTML = `
+        ${
+            poster
+                ? `<img src="${poster}" alt="${filme.title}">`
+                : `<div class="no-image">Imagem não disponível</div>`
+        }
+    `;
+
+    favoritesContainer.appendChild(movieCard);
 });
